0b4da78d04a64a18a6ad8ea26c4a22623a5c9a36
Niraj Tiwari <tniraj7@gmail.com>
2020-05-19 15:31:56 +0900

    add a makeUIView function which will return an instance of UIActivityIndicatorView with the style passed along

--- ActivityIdicator_SwiftUI/ActivityIdicator_SwiftUI/ActivityIndicatorView.swift
+++ ActivityIdicator_SwiftUI/ActivityIdicator_SwiftUI/ActivityIndicatorView.swift
@@ -6,4 +6,8 @@ struct ActivityIndicatorView: UIViewRepresentable {
     @Binding var animating: Bool
     let style: UIActivityIndicatorView.Style
     
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicatorView>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+    
 }
